f0fdcbf36859cd59626f9badc3aacfe0a06e617f
vmaeo <m4riavsl@gmail.com>
2026-06-02 14:53:29 +0300

    recommended file structure

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from gtgh-team3-compliance-assistant!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/gtgh_team3_compliance_assistant/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from gtgh_team3_compliance_assistant!")
