e857615d37350534cf1b3d0093d406bf159856a5
Aggelos Vouldis <agg2elos@gmail.com>
2026-06-06 15:46:07 +0300

    Added Storage Functionality
    Added storing/Storage.py for saving results and chunks to ChromaDB
    Also created test_storage.py for storage testing

---
+++ src/gtgh_team3_compliance_assistant/models/Search.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+
+class SearchInput(BaseModel):
+  query_embedding: list[float]
+  top_k: int = Field(default=5, gt=0)
+
+class SearchResult(BaseModel):
+  chunk_id: int
+  content: str
+  metadata: dict
+  distance: float
