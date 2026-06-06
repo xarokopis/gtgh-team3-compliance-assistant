37701f86923959607b77cfda1104f9b325979ba5
Aggelos Vouldis <agg2elos@gmail.com>
2026-06-06 14:26:35 +0300

    Embedding Created
    Embedding logic created in embedding file and test created inside tests/test_embedder.py

--- src/gtgh_team3_compliance_assistant/config.py
+++ src/gtgh_team3_compliance_assistant/config.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+# DIRS
 BASE_DIR = Path(__file__).resolve().parents[2]
 
 DATA_DIR = BASE_DIR / "data"
@@ -8,7 +9,11 @@ DATA_DIR_PDF = BASE_DIR / "data" / "pdf"
 RAW_DIR = DATA_DIR / "raw" / "eurlex"
 METADATA_DIR = DATA_DIR / "metadata"
 
+
 RAW_DIR.mkdir(parents=True, exist_ok=True)
 METADATA_DIR.mkdir(parents=True, exist_ok=True)
 
 METADATA_FILE = METADATA_DIR / "documents.json"
+
+# Embedding
+EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
